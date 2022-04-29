995a6368d402fa7539dee2754f1baa10782148bd
Csabbah <csabbah@ryerson.ca>
2022-04-29 15:37:34 -0400

    You can now delete notes via the preview page

--- public/assets/js/preview.js
+++ public/assets/js/preview.js
@@ -7,9 +7,11 @@
 // On page load, refresh the window so the data is up to date
 window.onpageshow = function (event) {
   if (event.persisted) {
+    window.location.pathname = '/notes/preview';
     window.location.reload(); //reload page if it has been loaded from cache
   }
 };
+// window.location.pathname = '/notes/#';
 
 // Extract the data from the API database
 var getNotes = async () => {
@@ -44,10 +46,21 @@ var getNotes = async () => {
         }
 
         handleNoteEvent(notes);
-
-        // To handle deleting items from the data base
-        deleteEvent();
       });
+
+      // To handle deleting items from the data base
+      // Add the event listeners for the trash buttons
+      var deleteBtns = document.querySelectorAll('.trash');
+      deleteBtns.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          // Extract the ID from the trash class which is associated with the note id
+          var noteId = e.target.classList[3].split('-')[1];
+          // Execute deleteNote() with the given id (uses delete method)
+          deleteNote(noteId);
+          updateEl();
+        });
+      });
+      return notes;
     }
     // If the above fetch fails...
   } catch (error) {
@@ -129,13 +142,38 @@ addBtn.addEventListener('click', () => {
   window.location.href = '/notes';
 });
 
-function deleteEvent() {
-  // var trashForm = document.querySelectorAll('.delete-form');
-  // trashForm.forEach((button) => {
-  //   button.addEventListener('click', () => {
-  //     document.querySelector('form').submit();
-  //   });
-  // });
+// Execute delete request and remove the appropriate element
+function deleteNote(id) {
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      accept: 'application/json; charset=utf-8',
+    },
+  });
+  // After data has been deleted from the JSON database, remove the element from the DOM
+  var allNotes = document.querySelectorAll('.singleNote-container');
+  allNotes.forEach((noteEl) => {
+    // Extract all the IDs of the active elements
+    var noteElId = noteEl.childNodes[0].classList[3].split('-')[1];
+    // The id in this case would be the element associated with the trash icon we click on
+    if (id == noteElId) {
+      noteEl.remove();
+    }
+  });
+}
+
+// Upon deleting a note, update all other elements to ensure consistency in label values
+// i.e. Update all links and labels (ids) starting value 0 and incrementing by 1
+function updateEl() {
+  var noteList = document.querySelectorAll('.note');
+  var trashList = document.querySelectorAll('.trash');
+  noteList.forEach((note, index) => {
+    note.href = `/notes/${index}`;
+    note.className = `note note-${index}`;
+  });
+  trashList.forEach((note, index) => {
+    note.className = `fa-solid fa-trash-can trash trash-${index}`;
+  });
 }
 
 getNotes();
